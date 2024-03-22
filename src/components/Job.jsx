5029b225f9e3a6e94530183e60329398a109e7e9
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-03-22 14:20:34 +0500

    Link Tag for Fast Navigation

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FaMapMarker } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Job = ({ jobData }) => {
 	const [showFullDesc, setShowFullDesc] = useState(false);
@@ -36,12 +37,12 @@ const Job = ({ jobData }) => {
 						<FaMapMarker className="inline text-lg mb-1 mr-1" />
 						{jobData.location}
 					</div>
-					<a
-						href={`/job/${jobData.id}`}
+					<Link
+						to={`/job/${jobData.id}`}
 						className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
 					>
 						Read More
-					</a>
+					</Link>
 				</div>
 			</div>
 		</div>
