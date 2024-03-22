8038a6f5337a31e84f3fc60a71f8eb31a9f976aa
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-03-22 14:11:34 +0500

    Created pages and Layouts section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import Hero from "./components/Hero";
-import JobListings from "./components/JobListings";
-import Navbar from "./components/Navbar";
-import HomeCards from "./components/HomeCards";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+	Route,
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<MainLayout />}>
+			<Route index element={<HomePage />} />
+		</Route>
+	)
+);
 
 const App = () => {
-	return (
-		<>
-			<Navbar />
-			<Hero />
-			<HomeCards /> {/* <!-- Developers and Employers --> */}
-			<JobListings />
-			<ViewAllJobs />
-		</>
-	);
+	return <RouterProvider router={router} />;
 };
 export default App;
+
+// <Navbar />
+// 			<Hero />
+// 			<HomeCards /> {/* <!-- Developers and Employers --> */}
+// 			<JobListings />
+// 			<ViewAllJobs />
