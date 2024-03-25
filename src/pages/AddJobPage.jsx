8dbe66c189b8579b2829456c4402db39e03faa1d
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-03-25 21:57:21 +0500

    React Toastify

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const AddJobPage = ({ addJobSubmit }) => {
 	const [title, setTitle] = useState("");
@@ -44,6 +45,7 @@ const AddJobPage = ({ addJobSubmit }) => {
 		};
 
 		addJobSubmit(newJob);
+		toast.success("Job Added Successfully.");
 		clearInputFields();
 
 		return navigate("/jobs");
